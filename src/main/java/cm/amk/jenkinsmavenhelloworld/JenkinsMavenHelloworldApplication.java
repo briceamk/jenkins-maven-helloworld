185e7b23b17dbf120a34bff618d0b6f3272fdecb
ambiandji <ambiandji@gmail.com>
2022-11-11 18:33:00 +0100

    initial commit

---
+++ src/main/java/cm/amk/jenkinsmavenhelloworld/JenkinsMavenHelloworldApplication.java
@@ -0,0 +1,14 @@
+package cm.amk.jenkinsmavenhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsMavenHelloworldApplication.class, args);
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/test/java/cm/amk/jenkinsmavenhelloworld/JenkinsMavenHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.amk.jenkinsmavenhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMavenHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
